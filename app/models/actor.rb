6d0acb9d5e28cbaddfb953222e207c37a0a21c5d
Michael Sawula <msawula@users.noreply.github.com>
2016-11-20 14:54:32 -0600

    added characters

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,6 @@
 class Actor < ActiveRecord::Base
+
+  #   - name: must be present; must be unique in combination with dob
+    validates :name, :presence => true, :uniqueness => { :scope => :dob}
+
 end
